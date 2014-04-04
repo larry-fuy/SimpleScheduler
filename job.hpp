720b9a7cee3440feb96ecd98b18d983326230c39
larry-fuy <larry.yong.fu@gmail.com>
2014-04-04 18:29:13 -0500

    add more in readme

--- job.hpp
+++ job.hpp
@@ -1,11 +1,29 @@
 // Jobs
 #pragma once
 #include<vector>
+#include<cstdlib>
 
-class Job {
-public:
-  Job() {};
+using namespace std;
+struct Job {
+    int length; // size
+    int start;  // staring time
+    int end;   // end time
+  };
+
+class Jobs {
+  int time;
+  int max_size; 
+  vector<Job> job_queue;
 
 public:
+  Jobs(int t) : time(t), max_size(10) {};
+
+  Jobs(int t, int s) : time(t), max_size(s)  {};
+
+  // generate the random size jobs set, however this can be read from files.
+  void generate();
+  // collect statistics
   void collect();
+  // get a job from the job sets
+  Job get_job(int i);
 };
--- simulator.hpp
+++ simulator.hpp
@@ -1,10 +1,12 @@
 #pragma once
+#include<fstream>
+#include<iostream>
 #include<vector>
 #include<algorithm>
 
+
 #include"job.hpp"
 #include"scheduler.hpp"
-#include"simulator.hpp"
 
 using namespace std;
 
@@ -19,14 +21,31 @@ private:
   size_t num_cpus;
 
   utilization_t util;
+  // for results
+  ofstream util_file;
+  vector<vector<int>> cluster;
+  vector<int> resources;
 
 public:
-  Simulator(Scheduler* s, Job* j, const int t, const size_t nn, const size_t nc) :
+  Simulator(Scheduler* s, Jobs* j, const int t, const size_t nn, const size_t nc) :
     sch(s), sim_time(t), num_node(nn), num_cpus(nc) {
-    // initialize the utilization
+    resources.resize(sim_time);
+
+    // utilization file
+    util_file.open("util.dat");
+
+    // cluster init
+    for (auto i = 0; i < num_node; ++i)
+      cluster.push_back(vector<int>(num_cpus, 0));
+
+    // resource input generate or  it can be read from files
+    for (auto i = 0; i < sim_time; ++i) 
+      resources[i] = rand() % num_node;
   };
 
-  ~Simulator() {};
+  ~Simulator() { 
+    util_file.close();
+};
 
   // run simulator
   void run();
