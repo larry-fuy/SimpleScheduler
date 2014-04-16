d2488d91245e7ed81e0a065377cf9fd3aca2d509
larry-fuy <larry.yong.fu@gmail.com>
2014-04-15 20:59:55 -0500

    change some parameters

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@
 // simulation parameters
 // Todo : read parameters from configuration file
 const int SIMU_TIME = 100;
-const size_t NUM_NODE = 16;
-const size_t NUM_CPUS = 16;
+const size_t NUM_NODE = 8;
+const size_t NUM_CPUS = 8;
 const size_t MAX_SIZE = 4;   // max size of the job, in real world no such limit
 
 int main() {
